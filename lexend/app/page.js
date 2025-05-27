d052fd691a9e60acb0a935a442a34bcac3e19371
Mohit Mittsl <mittalmohit2003@gmail.com>
2025-05-27 18:36:34 +0530

    edited title

--- lexend/app/page.js
+++ lexend/app/page.js
@@ -1,7 +1,7 @@
 import Home6 from "./(homes)/home-6/page";
 export const metadata = {
   title:
-    "Home 1 || Lexend - Full-featured, professional-looking software, saas and startup nextjs template.",
+    "Home 6 || Lexend - Full-featured, professional-looking software, saas and startup nextjs template.",
   description:
     "Lexend - Full-featured, professional-looking software, saas and startup nextjs template.",
 };
